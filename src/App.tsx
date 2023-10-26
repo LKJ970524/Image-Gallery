921e146261381b324deb690f744e210cfaed6776
LKJ970524 <dlrbwjd7096@naver.com>
2023-10-26 23:10:53 +0900

    드래그 드롭 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,46 @@
-import React from "react";
-import logo from "./logo.svg";
+import { useCallback, useState } from "react";
 import "./App.css";
+import ImageBox from "./components/ImageBox";
+import {useDropzone} from "react-dropzone"
 
 function App() {
+  const [imageList, setImageList] = useState<string[]>([]);
+
+  const onDrop = useCallback((acceptedFiles: string | any[]) => {
+    console.log(acceptedFiles);
+    
+    if (acceptedFiles.length) {
+      for (const file of acceptedFiles) {
+        const reader = new FileReader()
+
+        reader.readAsDataURL(file)
+        reader.onloadend = (event) => {
+          setImageList((prev) => [...prev, event.target?.result as string])
+        }
+      }
+    }
+  }, [])
+  const {getRootProps, getInputProps} = useDropzone({onDrop})
+
   return (
     <div className="container">
-      <div className="initial-box">
-        <div className="text-center">
-          이미지가 없습니다.<br />
-          이미지를 추가해주세요
-        </div>
-        <div className="plus-box">
+      <div className={"gallery-box " + (imageList.length > 0 && "row")}>
+        {
+          imageList.length === 0 &&
+          <div className="text-center">
+            이미지가 없습니다. <br />
+            이미지를 추가해주세요
+          </div>
+        }
+        {
+          imageList.map((el, idx) => <ImageBox key={el + idx} src={el} />)
+        }
+        <div className="plus-box"
+          {...getRootProps()}
+        >
+        <input
+          {...getInputProps()}
+          />
           +
         </div>
       </div>
